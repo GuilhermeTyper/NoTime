a317e05aa7519d744196b434860b0d5c83ff4da8
guilherme <270210.gui@gmail.com>
2017-08-10 20:50:28 -0300

    "dealing with errors"

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,12 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases", trocaFrase);
+    $.get("http://localhost:3000/frases", trocaFrase).fail(function() {   
+        $("#erro").show();
+        setTimeout(function() {
+            $("#erro").toggle();
+        },1500);
+    });
 }
     function trocaFrase(data) {
         var frase = $(".frase");
