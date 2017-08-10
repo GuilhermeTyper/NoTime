964657071e0ddf522a67fedfc067aba66d19b424
guilherme <270210.gui@gmail.com>
2017-08-09 23:40:19 -0300

    "arranging time and text"

--- public/js/frase.js
+++ public/js/frase.js
@@ -2,10 +2,14 @@ $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
     $.get("http://localhost:3000/frases", trocaFrase);
-
+}
     function trocaFrase(data) {
         var frase = $(".frase");
         var numeroAleatorio = Math.floor(Math.random() * data.length);//sortindo e multiplicando o json mais o seu tamnho para limita-lo
         frase.text(data[numeroAleatorio].texto);//pegando a propriedade texto e transformando em texto tudo aleatoriamente
-    }
+        atualizaTamanhoFrase();
+        atualizaTempoInicial(data[numeroAleatorio].tempo);
+        console.log(data);
+
+
 }
