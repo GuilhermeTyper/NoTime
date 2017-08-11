539b1d7f40261fd342818a02158e7ac4c1a705ac
guilherme <270210.gui@gmail.com>
2017-08-10 23:38:56 -0300

    "commenting on the new features"

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,5 +1,5 @@
-$("#botao-frase").click(fraseAleatoria);
-$("#botao-frase-id").click(buscarFrase);
+$("#botao-frase").click(fraseAleatoria);//caso botã-frase seja clicado irá execulta uma função 
+$("#botao-frase-id").click(buscarFrase); //caso botã-frase-id seja clicado irá execulta uma função 
 
 function fraseAleatoria() {
 
@@ -24,23 +24,23 @@ function fraseAleatoria() {
 }
 
 function buscarFrase() {
-    $("#spinner").toggle();
-    var fraseId = $("#frase-id").val();
-    console.log(fraseId);
-    var dados = {id: fraseId};
+    $("#spinner").toggle();//mostrando o spinner
+    var fraseId = $("#frase-id").val();//pegando o id do input do html e atribuino a uma variavel
+    console.log(fraseId);//mostrando o que o usuario digitou pelo input no console
+    var dados = {id: fraseId};// criando um objeto que irá criar um id e atribuindo a uma variavel
     $.get("http://localhost:3000/frases",dados,trocaFrase).fail(function() {
-        $("#erro").toggle();
+        $("#erro").toggle();//caso apresente erro a mensagem irá ser colocada para o usuario
         setTimeout(function() {
-            $("#erro").toggle();
+            $("#erro").toggle();  //após 2 minutos a menssagem será retirada da tela do usuario 
         }, 2000);
     }).always(function() {
-        $("#spinner").toggle();
+        $("#spinner").toggle();// indepedente de qualquer coisa o spinner irá aparecer para o usuario
     });
 }
 
 function trocaFrase(data) {
-    var frase = $(".frase");
-    frase.text(data.texto);
-    atualizaTamanhoFrase();
-    atualizaTempoInicial(data.tempo);
+    var frase = $(".frase");//pegando a classe frase do html e atribuindo a uma variavel
+    frase.text(data.texto);//pegando o texto do arquivo data e atribuindo a função text e  fazendo ela ser atribuida a função frase
+    atualizaTamanhoFrase();//chamando função que se encontra no arquivo main.js
+    atualizaTempoInicial(data.tempo);//chamando função que se encontra no arquivo main.js
 }
