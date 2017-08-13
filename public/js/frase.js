fb3f34e4b819cf8334175899e5b3faf9c69097b3
guilherme <270210.gui@gmail.com>
2017-08-13 01:20:38 -0300

    "#1 issue fixed"

--- public/js/frase.js
+++ public/js/frase.js
@@ -2,10 +2,8 @@ $("#botao-frase").click(fraseAleatoria);//caso botã-frase seja clicado irá exe
 $("#botao-frase-id").click(buscarFrase); //caso botã-frase-id seja clicado irá execulta uma função 
 
 function fraseAleatoria() {
-
     $("#spinner").toggle();// assim que uma requisição for chamada o spinner irá aparecer 
-
-    $.get("http://localhost:3000/frases", trocaFrase).fail(function() {   
+    $.get("http://localhost:3000/frases", fraseSortida).fail(function() {   
         $("#erro").show();//caso o usuario esteja sem internet ira acontecer esse evento
         setTimeout(function() {
             $("#erro").toggle();
@@ -14,13 +12,14 @@ function fraseAleatoria() {
         $("#spinner").toggle();//mesmo se caso aparecer algum erro o spinner irá aparecer
     });
 }
-    function trocaFrase(data) {
+    function fraseSortida(data) {
         var frase = $(".frase");
         var numeroAleatorio = Math.floor(Math.random() * data.length);//sortindo e multiplicando o json mais o seu tamnho para limita-lo
         frase.text(data[numeroAleatorio].texto);//pegando a propriedade texto e transformando em texto tudo aleatoriamente
         atualizaTamanhoFrase();//essa função está vindo do main.js
         atualizaTempoInicial(data[numeroAleatorio].tempo);//essa função está vindo do main.js
         console.log(data);//verificando se o json está sendo aplicado corretamente no app
+        
 }
 
 function buscarFrase() {
